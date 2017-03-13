16c7d5d14f300b9bc0132bcda5e148f868f4a937
claydev <saad8kw@gmail.com>
2017-03-14 01:22:09 +0300

    Update onmessage.js

--- controllers/onmessage.js
+++ controllers/onmessage.js
@@ -9,7 +9,7 @@ bot.on('message', msg => {
     }, (err, count) => {
         if (count > 0) {
             bot.kickChatMember(msg.chat.id, msg.from.id);
-            bot.sendMessage(msg.chat.id, `*${msg.from.first_name}* Is Globally Banned!`, {reply_to_message_id: msg.message_id, parse_mode: 'Markdown'});
+            bot.sendMessage(msg.chat.id, `*${msg.from.first_name}* محظور عام!`, {reply_to_message_id: msg.message_id, parse_mode: 'Markdown'});
         } else {
             // Some Random User Talking
         }
