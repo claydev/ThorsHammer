ab3ff2ab7a83fda816a3c3f7c8674f66ca5900af
claydev <saad8kw@gmail.com>
2017-03-14 01:21:40 +0300

    Update onjoin.js

--- controllers/onjoin.js
+++ controllers/onjoin.js
@@ -9,7 +9,7 @@ bot.on('new_chat_participant', msg => {
     }, (err, count) => {
         if (count > 0) {
             bot.kickChatMember(msg.chat.id, msg.new_chat_participant.id);
-            bot.sendMessage(msg.chat.id, `${msg.new_chat_participant.first_name} Is Globally Banned!`, {reply_to_message_id: msg.message_id, parse_mode: 'Markdown'});
+            bot.sendMessage(msg.chat.id, `${msg.new_chat_participant.first_name} محظور عام!`, {reply_to_message_id: msg.message_id, parse_mode: 'Markdown'});
         }
     });
 });
@@ -17,7 +17,7 @@ bot.on('new_chat_participant', msg => {
 bot.on('new_chat_participant', msg => {
     bot.getMe().then(me => {
         if (msg.new_chat_participant.username == me.username) {
-            bot.sendMessage(msg.chat.id, `<code>Hello There! I Am A Group Administration Bot, Hit The Button Below To Learn More About Me!</code>`, {parse_mode: 'HTML', reply_markup: {inline_keyboard: [[{ text: `Start Me`, url: `https://telegram.me/${me.username}?start`}]]}});
+            bot.sendMessage(msg.chat.id, `<code>مرحبا انا بوت ادارة المجموعات , اصغط على الزر في الاسفل لرؤية المزيد من المعلومات</code>`, {parse_mode: 'HTML', reply_markup: {inline_keyboard: [[{ text: `Start Me`, url: `https://telegram.me/${me.username}?start`}]]}});
         }
     });
 });
